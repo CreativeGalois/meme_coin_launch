a2e5cc7102813162225fe22818f7edd75bb37558
Aoi <ht.so.ik0102@gmail.com>
2021-11-23 20:37:23 +0700

    create new file token.sol

---
+++ contracts/Token.sol
@@ -0,0 +1,14 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract Token is ERC20 {
+    string public constant NAME = "Meme";
+    string public constant SYMBOL = "MEME";
+    uint256 public constant TOTAL_SUPPLY = 1000000000000;
+
+    constructor(address _manager) ERC20(NAME, SYMBOL) {
+        _mint(_manager, TOTAL_SUPPLY);
+    }
+}

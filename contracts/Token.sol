01a1bf67f5574a2d93693b6d3ae44629e40bf094
Aoi <ht.so.ik0102@gmail.com>
2021-11-26 23:55:41 +0700

    remove commnet

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -6,7 +6,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 contract Token is ERC20 {
     string public constant NAME = "Meme";
     string public constant SYMBOL = "MEME";
-    uint256 public constant TOTAL_SUPPLY = 1000000000000; //1000000000000
+    uint256 public constant TOTAL_SUPPLY = 1000000000000;
 
     constructor() ERC20(NAME, SYMBOL) {
         _mint(msg.sender, 1000 * 10 ** 18);

9de9e6371bf3718751098a58b724365883dbb343
Aoi <ht.so.ik0102@gmail.com>
2021-11-26 07:35:52 +0700

    beforeeach function

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -6,6 +6,9 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 contract Token is ERC20 {
     string public constant NAME = "Meme";
     string public constant SYMBOL = "MEME";
+    uint256 public constant TOTAL_SUPPLY = 1000000000000;
 
-    constructor() ERC20(NAME, SYMBOL) {}
+    constructor() ERC20(NAME, SYMBOL) {
+        _mint(address(this), TOTAL_SUPPLY);
+    }
 }
--- test/index.ts
+++ test/index.ts
@@ -1,16 +1,19 @@
 import { expect, use } from "chai";
 import { ethers } from "hardhat";
-import solidity from "ethereum-waffle";
+import { solidity } from "ethereum-waffle";
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 
-// use(solidity);
+use(solidity);
 
 describe("Token Management", function () {
-  let owner;
+  let owner: SignerWithAddress;
   let addr1;
   let addr2;
   let addrs;
+  const TOTAL_SUPPLY = 1000000000000;
 
   let tokenManagementContract;
+  let tokenFactory;
   let tokenContract;
 
   let tokenMangementSupply;
@@ -19,7 +22,28 @@ describe("Token Management", function () {
   beforeEach(async () => {
     [owner, addr1, addr2, ...addrs] = await ethers.getSigners();
 
-    tokenContract = await ethers.getContractFactory("Token");
+    tokenFactory = await ethers.getContractFactory("Token");
+    tokenContract = await tokenFactory.deploy();
+
+    const TokenManagementContract = await ethers.getContractFactory(
+      "TokenManagement"
+    );
+    tokenManagementContract = await TokenManagementContract.deploy(
+      tokenContract.address,
+      owner.toString(),
+      addr1.toString(),
+      addr2.toString()
+    );
+
+    await tokenContract.transfer(
+      tokenManagementContract.address,
+      ethers.utils.parseEther(TOTAL_SUPPLY.toString())
+    );
+
+    tokenMangementSupply = await tokenContract.balanceOf(
+      tokenManagementContract.address
+    );
+    tokensPerBnb = await tokenManagementContract.TOKENSPERBNB();
   });
 
   it("Should return the new greeting once it's changed", async function () {

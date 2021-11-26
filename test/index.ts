6919f37e355d405759b489a14e5cdff01fe39186
Aoi <ht.so.ik0102@gmail.com>
2021-11-26 07:08:05 +0700

    compile successfully

--- test/index.ts
+++ test/index.ts
@@ -1,7 +1,27 @@
-import { expect } from "chai";
+import { expect, use } from "chai";
 import { ethers } from "hardhat";
+import solidity from "ethereum-waffle";
 
-describe("Token", function () {
+// use(solidity);
+
+describe("Token Management", function () {
+  let owner;
+  let addr1;
+  let addr2;
+  let addrs;
+
+  let tokenManagementContract;
+  let tokenContract;
+
+  let tokenMangementSupply;
+  let tokensPerBnb;
+
+  beforeEach(async () => {
+    [owner, addr1, addr2, ...addrs] = await ethers.getSigners();
+
+    tokenContract = await ethers.getContractFactory("Token");
+  });
+
   it("Should return the new greeting once it's changed", async function () {
     const ownerAddress = process.env.OWNERADDRESS!;
     const Token = await ethers.getContractFactory("Token");
